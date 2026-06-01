3f9e503c43d91b3c4316ef2fe5fdac4bafe44419
antomfdez <antonio.mesa.fdez@gmail.com>
2026-06-01 02:56:56 +0200

    Indentation change to fit linux criteria (8char tab nonspace)

--- lua/antomfdez/set.lua
+++ lua/antomfdez/set.lua
@@ -3,10 +3,10 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.expandtab = false
 
 vim.opt.smartindent = true
 
@@ -28,4 +28,4 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80,100"
